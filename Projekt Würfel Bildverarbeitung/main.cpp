945e6fddef60ebcc6671afdcd5f9c7c9d300ea2a
Frehner Mauro <maurofrehner@icloud.com>
2024-03-07 10:56:27 +0100

    Initial Commit

---
+++ Projekt Würfel Bildverarbeitung/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Projekt Würfel Bildverarbeitung
+//
+//  Created by Mauro Frehner on 07.03.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
